4f3838d7d79b8f15fb0b69f8412e895ae5929034
ganesh96-lab <ganeshghodke7050@gmail.com>
2023-01-23 01:07:25 +0530

    bug 121

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -11,6 +11,10 @@ public class App
         System.out.println( "Hello World! welcome pune in india" );
 	sysout("I'm main ");
 	sysout("I'm slave");
+	
+	//BUG-121
+	public void abc(){
+	}
     }
 //122-BUG
 public bug122(){
